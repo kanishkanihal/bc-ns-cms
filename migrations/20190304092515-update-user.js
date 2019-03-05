a74d151098ff32b85ac0e4f8316d228c5ce11a33
Kanishka Wickramarathna <kanishka.wickramarathna@netstarter.com>
2019-03-05 07:25:09 +0530

    Add columns for multi user functions.

---
+++ migrations/20190304092515-update-user.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface
+      .addColumn("Clients", "bcUserId", {
+        type: Sequelize.STRING(64),
+        allowNull: true
+      })
+      .then(() => {
+        queryInterface.addColumn("Clients", "bcClientId", {
+          type: Sequelize.STRING(64),
+          allowNull: true
+        });
+      })
+      .then(
+        queryInterface.addColumn("Clients", "bcClientSecret", {
+          type: Sequelize.STRING(64),
+          allowNull: true
+        })
+      )
+      .then(
+        queryInterface.addColumn("Clients", "hashCode", {
+          type: Sequelize.STRING(64),
+          allowNull: false
+        })
+      );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface
+      .removeColumn("Clients", "bcUserId")
+      .then(queryInterface.removeColumn("Clients", "bcClientId"))
+      .then(queryInterface.removeColumn("Clients", "bcClientSecret"))
+      .then(queryInterface.removeColumn("Clients", "hashCode"));
+  }
+};
--- seeders/20190304041640-client-test-data.js
+++ seeders/20190304041640-client-test-data.js
@@ -1,16 +1,25 @@
-'use strict';
+"use strict";
+
+const crypto = require("crypto");
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Clients', [{
-      id:1,
-      email: 'jhon.doe@netstarter.com',
-      createdAt:  new Date(),
-      updatedAt : new Date(),
-    }], {});
+    return queryInterface.bulkInsert(
+      "Clients",
+      [
+        {
+          id: 1,
+          email: "jhon.doe@netstarter.com",
+          hashCode: crypto.randomBytes(32).toString("hex"),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Clients', null, {});
+    return queryInterface.bulkDelete("Clients", null, {});
   }
 };
