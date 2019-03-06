a2df25dea4461525324ff5b8005b5ff4f5f02209
Kanishka Wickramarathna <kanishka.wickramarathna@netstarter.com>
2019-03-06 14:16:25 +0530

    Format the code

--- controllers/cmsController.js
+++ controllers/cmsController.js
@@ -1,41 +1,36 @@
-const db = require('../models/index')
-const block = db.Block
+const db = require("../models/index");
+const block = db.Block;
 module.exports = {
   findAll: async (req, res, next) => {
-    block.findAll().then(
-        result => res.json(result)
-      )  
+    var result = await block.findAll();
+    res.json(result);
   },
-  findById: async (req, res, next) => { 
-    
+  findById: async (req, res, next) => {
     var result = await block.findAll({
       where: {
         id: req.params.id
       }
     });
-
-    res.json(result)
+    res.json(result);
   },
-  create: async(req, res, next)=>{
-    block.create(req.body).then(
-      result => res.json(result)
-    )
+  create: async (req, res, next) => {
+    var result = await block.create(req.body);
+    res.json(result);
   },
-  update: async(req, res, next)=>{
-    block.update(req.body,{
-      where : {
-        id: req.params.id 
+  update: async (req, res, next) => {
+    var result = await block.update(req.body, {
+      where: {
+        id: req.params.id
       }
-    }).then(
-      result => res.json(result)
-    )
+    });
+    res.json(result);
   },
-  delete: async(req, res, next)=>{
+  delete: async (req, res, next) => {
     var result = await block.destroy({
       where: {
         id: req.body.id
       }
-    })
-    res.json(result)
+    });
+    res.json(result);
   }
 };
